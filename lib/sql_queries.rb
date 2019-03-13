8641740e20830069c4ca3d224fcb2fa7bf02227f
J. Singleton <jennifer.singleton@gmail.com>
2019-03-13 00:52:58 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,10 @@ HAVING SUM(pledges.amount) >= projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT users.name, pledges.amount FROM users
+JOIN pledges ON users.id = pledges.user_id
+GROUP BY pledges.user_id
+ORDER BY pledges.amount"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
