0110ecddb488b6d954c3981b1671b541ce7a552a
J. Singleton <jennifer.singleton@gmail.com>
2019-03-13 00:40:16 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -6,7 +6,7 @@ ORDER BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT users.name, users.age, SUM(pledges.amount) FROM projects
+"SELECT users.name, users.age, SUM(pledges.amount) FROM users
 JOIN pledges ON users.id = pledges.user_id
 GROUP BY pledges.user_id
 ORDER BY users.name;"
